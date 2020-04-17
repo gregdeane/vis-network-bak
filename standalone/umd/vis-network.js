74c60f88fa8bba431785afb804e2fde4ff0772ff
Greg Deane <greg.james.deane@zalando.de>
2020-04-17 12:25:21 +0200

    zoom and sticky tooltip latest

--- standalone/umd/vis-network.js
+++ standalone/umd/vis-network.js
@@ -4,8 +4,8 @@
  *
  * A dynamic, browser-based visualization library.
  *
- * @version 7.6.1
- * @date    2020-04-17T10:16:25.671Z
+ * @version 0.0.0-no-version
+ * @date    2020-04-17T10:22:43.258Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
