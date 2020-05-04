730dfa2558a34d7f2b20e9d62f78cfa79553bf9a
Greg Deane <greg.james.deane@zalando.de>
2020-05-04 08:21:06 +0200

    fetch upstream

--- standalone/umd/vis-network.min.js
+++ standalone/umd/vis-network.min.js
@@ -5,7 +5,7 @@
  * A dynamic, browser-based visualization library.
  *
  * @version 0.0.0-no-version
- * @date    2020-04-30T10:26:02.984Z
+ * @date    2020-05-04T06:16:42.911Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
